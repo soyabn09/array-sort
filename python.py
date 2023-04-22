61f4390496c781310a133f0c3a6ea9026d8f32a2
Soyab Nandhla <admin@soyabnandhla.me>
2023-04-22 15:00:08 +0100

    Update python.py

--- python.py
+++ python.py
@@ -1,30 +1,25 @@
-words = [] # A list of strings
+words = []
 def main():
-    choose=input("\nA)Add Word \nB)Sort \nC)Reverse Sort \nD)Clear \nE)Length of Array \nF)Exit \n\nPlease enter a letter from the above: ").upper() # User gets to make a choice
-    if choose=="A": # User chooses A and can enter a word and that will be added to the list
-        word=input("\nPlease enter a word: ")
-        words.append(word)
-        print()
-        [print(i) for i in words]
-        main()
-    elif choose=="B": # User chooses B and the list is sorted alphabetically and outputted
-        words.sort()
-        [print(i) for i in words]
-        main()
-    elif choose=="C": # User chooses C and the list is sorted in reverse and outputted
-        words.reverse()
-        [print(i) for i in words]
-        main()
-    elif choose=="D": # User chooses D and the list is cleared
-        words.clear()
-        [print(i) for i in words]
-        main()
-    elif choose=="E": # User chooses E and the length of the list is outputted
-        print('\n{0}\n'.format(len(words)))
-        [print(i) for i in words]
-        main()
-    elif choose=="F": # User chooses F and the program exits
-        exit()
-    else: # User doesn't make any of the above choices it will be repeated again
-        main()
-main()
+    while True:
+        choose = input("\nA)Add Word \nB)Sort \nC)Reverse Sort \nD)Clear \nE)Length of Array \nF)Exit \n\nPlease enter a letter from the above: ").upper()
+        if choose == "A":
+            word = input("\nPlease enter a word: ")
+            words.append(word)
+            print()
+            [print(i) for i in words]
+        elif choose == "B":
+            words.sort()
+            [print(i) for i in words]
+        elif choose == "C":
+            words.reverse()
+            [print(i) for i in words]
+        elif choose == "D":
+            words.clear()
+            [print(i) for i in words]
+        elif choose == "E":
+            print('\n{0}\n'.format(len(words)))
+            [print(i) for i in words]
+        elif choose == "F":
+            exit()
+        else:
+            continue
